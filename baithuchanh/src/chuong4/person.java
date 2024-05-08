3d8e4f81f8eb60ff8997bf45dc5ea5cce5286504
dinhtrung1301 <nguyendinhtrunggg1301@gmail.com>
2024-05-08 11:09:38 +0700

    .

---
+++ baithuchanh/src/chuong4/person.java
@@ -0,0 +1,19 @@
+package chuong4;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class person {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayListiIntegers = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        
+        System.out.println(x:"Nhap so phan tu cua Array: ");
+        int n = scanner.nextInt();
+
+    for (int i = 0; i < 100; i++) {
+        System.out.print("Nhap phan tu " + i + ": ");
+        number = scanner.nextInt();
+        arrayListiIntegers.add(number);
+    }
+    }
+}
